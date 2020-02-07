87449bb7e9428bb5ea03f89d0eaaa3ec75f006c8
Pascal Grimaud <pascalgrimaud@gmail.com>
2020-02-07 21:16:18 +0100

    Add jdl

--- test-integration/scripts/11-generate-entities.sh
+++ test-integration/scripts/11-generate-entities.sh
@@ -12,7 +12,7 @@ fi
 #-------------------------------------------------------------------------------
 moveEntity() {
     local entity="$1"
-    cp "$JHI_SAMPLES"/.jhipster/"$entity".json "$JHI_FOLDER_APP"/.jhipster/
+    cp "$JHI_SAMPLES"/.jhipster/"$entity".jh "$JHI_FOLDER_APP"/.jhipster/
 }
 
 prepareFolder() {
@@ -27,121 +27,8 @@ if [[ $JHI_REPO != "" ]]; then
     prepareFolder
 fi
 
-if [[ ("$JHI_ENTITY" == "mongodb") || ("$JHI_ENTITY" == "couchbase") ]]; then
-    moveEntity DocumentBankAccount
-
-    moveEntity FieldTestEntity
-    moveEntity FieldTestMapstructEntity
-    moveEntity FieldTestServiceClassEntity
-    moveEntity FieldTestServiceImplEntity
-    moveEntity FieldTestInfiniteScrollEntity
-    moveEntity FieldTestPaginationEntity
-
-elif [[ "$JHI_ENTITY" == "cassandra" ]]; then
-    moveEntity CassBankAccount
-
-    moveEntity CassTestEntity
-    moveEntity CassTestMapstructEntity
-    moveEntity CassTestServiceClassEntity
-    moveEntity CassTestServiceImplEntity
-
-elif [[ "$JHI_ENTITY" == "micro" ]]; then
-    moveEntity MicroserviceBankAccount
-    moveEntity MicroserviceOperation
-    moveEntity MicroserviceLabel
-
-    moveEntity FieldTestEntity
-    moveEntity FieldTestMapstructEntity
-    moveEntity FieldTestServiceClassEntity
-    moveEntity FieldTestServiceImplEntity
-    moveEntity FieldTestInfiniteScrollEntity
-    moveEntity FieldTestPaginationEntity
-
-elif [[ "$JHI_ENTITY" == "uaa" ]]; then
-    moveEntity FieldTestEntity
-    moveEntity FieldTestMapstructEntity
-    moveEntity FieldTestServiceClassEntity
-    moveEntity FieldTestServiceImplEntity
-    moveEntity FieldTestInfiniteScrollEntity
-    moveEntity FieldTestPaginationEntity
-
-elif [[ "$JHI_ENTITY" == "sqllight" ]]; then
-    moveEntity BankAccount
-    moveEntity Label
-    moveEntity Operation
-
-elif [[ "$JHI_ENTITY" == "sqlfull" ]]; then
-    moveEntity BankAccount
-    moveEntity Label
-    moveEntity Operation
-    moveEntity Place
-    moveEntity Division
-
-    moveEntity FieldTestEntity
-    moveEntity FieldTestMapstructEntity
-    moveEntity FieldTestServiceClassEntity
-    moveEntity FieldTestServiceImplEntity
-    moveEntity FieldTestInfiniteScrollEntity
-    moveEntity FieldTestPaginationEntity
-
-    moveEntity TestEntity
-    moveEntity TestMapstruct
-    moveEntity TestServiceClass
-    moveEntity TestServiceImpl
-    moveEntity TestInfiniteScroll
-    moveEntity TestPagination
-    moveEntity TestManyToOne
-    moveEntity TestManyToMany
-    moveEntity TestManyRelPaginDTO
-    moveEntity TestOneToOne
-    moveEntity TestCustomTableName
-    moveEntity TestTwoRelationshipsSameEntity
-    moveEntity SuperMegaLargeTestEntity
-
-    moveEntity EntityWithDTO
-    moveEntity EntityWithPagination
-    moveEntity EntityWithPaginationAndDTO
-    moveEntity EntityWithServiceClass
-    moveEntity EntityWithServiceClassAndDTO
-    moveEntity EntityWithServiceClassAndPagination
-    moveEntity EntityWithServiceClassPaginationAndDTO
-    moveEntity EntityWithServiceImpl
-    moveEntity EntityWithServiceImplAndDTO
-    moveEntity EntityWithServiceImplAndPagination
-    moveEntity EntityWithServiceImplPaginationAndDTO
-
-    moveEntity MapsIdParentEntityWithoutDTO
-    moveEntity MapsIdChildEntityWithoutDTO
-    moveEntity MapsIdParentEntityWithDTO
-    moveEntity MapsIdChildEntityWithDTO
-    moveEntity MapsIdUserProfileWithDTO
-
-elif [[ "$JHI_ENTITY" == "sql" ]]; then
-    moveEntity BankAccount
-    moveEntity Label
-    moveEntity Operation
-
-    moveEntity FieldTestEntity
-    moveEntity FieldTestMapstructEntity
-    moveEntity FieldTestServiceClassEntity
-    moveEntity FieldTestServiceImplEntity
-    moveEntity FieldTestInfiniteScrollEntity
-    moveEntity FieldTestPaginationEntity
-
-    moveEntity EntityWithDTO
-    moveEntity EntityWithPagination
-    moveEntity EntityWithPaginationAndDTO
-    moveEntity EntityWithServiceClass
-    moveEntity EntityWithServiceClassAndDTO
-    moveEntity EntityWithServiceClassAndPagination
-    moveEntity EntityWithServiceClassPaginationAndDTO
-    moveEntity EntityWithServiceImpl
-    moveEntity EntityWithServiceImplAndDTO
-    moveEntity EntityWithServiceImplAndPagination
-    moveEntity EntityWithServiceImplPaginationAndDTO
-
-    moveEntity MapsIdUserProfileWithDTO
-
+if [[ "$JHI_ENTITY" == "21points" || "$JHI_ENTITY" == "blog" ]]; then
+    moveEntity $JHI_ENTITY
 fi
 
 #-------------------------------------------------------------------------------
---
+++ test-integration/scripts/13-generate-entities-with-jdl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+source $(dirname $0)/00-init-env.sh
+
+cd "$JHI_FOLDER_APP"
+jhipster import-jdl *.jdl --no-insight
+
+#-------------------------------------------------------------------------------
+# Check folder where the app is generated
+#-------------------------------------------------------------------------------
+ls -al "$JHI_FOLDER_APP"
+git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
