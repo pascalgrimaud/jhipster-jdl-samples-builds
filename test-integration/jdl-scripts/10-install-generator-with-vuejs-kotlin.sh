ab975062b8a48e4cc662979fb2a651820cbcbac8
Pascal Grimaud <pascalgrimaud@gmail.com>
2020-02-07 20:12:36 +0100

    try to fix build

--- test-integration/jdl-scripts/10-install-generator-with-vuejs-kotlin.sh
+++ test-integration/jdl-scripts/10-install-generator-with-vuejs-kotlin.sh
@@ -9,15 +9,3 @@ source $(dirname $0)/00-init-env.sh
 npm install -g generator-jhipster@v6.6.0
 npm install -g generator-jhipster-vuejs@v1.4.0
 npm install -g generator-jhipster-kotlin@v1.4.0
-
-#-------------------------------------------------------------------------------
-# Override config
-#-------------------------------------------------------------------------------
-
-# replace 00-init-env.sh
-cp "$JHI_CLONED"/test-integration/jdl-scripts/00-init-env.sh "$JHI_HOME"/test-integration/scripts/
-
-# copy all samples
-cp -R "$JHI_CLONED"/test-integration/samples/* "$JHI_HOME"/test-integration/samples/
-mkdir "$JHI_HOME"/test-integration/jdl-samples
-cp -R "$JHI_CLONED"/test-integration/jdl-samples/* "$JHI_HOME"/test-integration/jdl-samples/
