c8dfc04ba69a706ab75880c505f1a61fdeb081cd
Pascal Grimaud <pascalgrimaud@gmail.com>
2020-02-07 22:18:11 +0100

    Install correct version

--- test-integration/scripts/10-install-generator-with-vuejs-kotlin.sh
+++ test-integration/scripts/10-install-generator-with-vuejs-kotlin.sh
@@ -6,6 +6,6 @@ source $(dirname $0)/00-init-env.sh
 #-------------------------------------------------------------------------------
 # Install JHipster Dependencies and Server-side library
 #-------------------------------------------------------------------------------
-npm install -g generator-jhipster@v6.6.0
-npm install -g generator-jhipster-vuejs@v1.4.0
-npm install -g generator-jhipster-kotlin@v1.4.0
+npm install -g generator-jhipster@6.6.0
+npm install -g generator-jhipster-vuejs@1.4.0
+npm install -g generator-jhipster-kotlin@1.4.0
